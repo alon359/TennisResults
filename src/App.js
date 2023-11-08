02236e625c3a3535d796f317407568ae7dba8f3e
alon <alon359@gmail.com>
2023-11-08 10:58:36 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import GalitComponent from './components/galit-component';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
 }
 
+componentDidUpdate(){
+  console.log('now')
+  if(this.state.count === 5)
+  {
+    this.setState.count = 0;
+  }
+}
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div>
+        
+        <GalitComponent />
+      
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/galit-component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+class GalitComponent extends Component {
+
+    render() 
+    {
+        return(
+            <div>
+                <h1>Galit is sexy as hell with ciggareas</h1>
+            </div>
+        );
+    }
+}
+
+export default GalitComponent;
